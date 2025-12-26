66f7a83df0c8a8355d02b71077ff3a28f45bdf46
phankiet <phanduykiet1@gmail.com>
2025-12-26 11:18:58 +0700

    fix

--- scifun-api/src/main/java/com/sc/scifunapi/config/SecurityConfig.java
+++ scifun-api/src/main/java/com/sc/scifunapi/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
                 "http://localhost:3000",
-                "http://192.168.11.124:3000"
+                "http://192.168.11.124:3000",
+                "https://admin-scifun.onrender.com"
         ));
 
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
